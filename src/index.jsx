603a2ba5f83188acfd1c4d0cb9930ab5839438fe
Nyarrha <paradisquentin@yahoo.fr>
2023-08-09 14:49:12 +0200

    Finition CSS

--- src/index.jsx
+++ src/index.jsx
@@ -12,28 +12,31 @@ import Error from './pages/Error/Error'
 import About from './pages/About/About'
 import Appart from './pages/Appart/Appart'
 import Footer from './components/Footer/Footer'
+import './index.scss'
 
 // Création page React index
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      {/* Appel component Header */}
-      <Header />
-      {/* Création liste routes */}
-      <Routes>
-        {/* Route page Home */}
-        <Route path="/" element={<Home />} />
-        {/* Route page About */}
-        <Route path="/about" element={<About />} />
-        {/* Route page détails logement choisi */}
-        <Route path="/apparts/:id" element={<Appart />} />
-        {/* Route page Error */}
-        <Route path="/error" element={<Error />} />
-        {/* Redirection page Error pour gestion mauvaises URL */}
-        <Route path="*" element={<Navigate to="/error" />} />
-      </Routes>
-      {/* Appel component Footer */}
-      <Footer />
+      <main className="main">
+        {/* Appel component Header */}
+        <Header />
+        {/* Création liste routes */}
+        <Routes>
+          {/* Route page Home */}
+          <Route path="/" element={<Home />} />
+          {/* Route page About */}
+          <Route path="/about" element={<About />} />
+          {/* Route page détails logement choisi */}
+          <Route path="/apparts/:id" element={<Appart />} />
+          {/* Route page Error */}
+          <Route path="/error" element={<Error />} />
+          {/* Redirection page Error pour gestion mauvaises URL */}
+          <Route path="*" element={<Navigate to="/error" />} />
+        </Routes>
+        {/* Appel component Footer */}
+        <Footer />
+      </main>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
